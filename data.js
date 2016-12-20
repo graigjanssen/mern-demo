7f027b29624aaf865a7e7084a7b7b392f2111db0
Graig Janssen <graig.janssen@gmail.com>
2016-12-20 15:06:34 -0500

    Bring in sample components, dummy data

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+  { id: 1, author: 'Bryan', text: 'Wow this is neat!'},
+  { id: 2, author: 'You', text: 'youre __right!__'},
+  { id: 3, author: 'Rick', text: '60 smidgeons for a broken defrakulator!'}
+]
+
+module.exports = data;
